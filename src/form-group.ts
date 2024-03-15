fa483aaf4c725902f2e651f37680d3a902097ffe
Colin Fahrion <colin.fahrion@gmail.com>
2024-03-15 16:32:25 -0700

    feat: add remove disabled with groups

--- src/form-group.ts
+++ src/form-group.ts
@@ -3,12 +3,19 @@ export default class FormGroup extends HTMLElement {
 		super();
 	}
 
+	private fields = this.querySelectorAll("input, select, textarea");
+
 	public connectedCallback(): void {
 		console.log("connected");
 
 		const form = this.closest("form");
 		const watchId = this.getAttribute("watch");
 		if (form && watchId) {
+			// disable all fields in the group
+			this.fields.forEach((field) => {
+				field.setAttribute("disabled", "");
+			});
+			// Set up event listeners
 			const watchField = form.querySelector(`#${watchId}`);
 			if (watchField instanceof HTMLInputElement || watchField instanceof HTMLSelectElement || watchField instanceof HTMLTextAreaElement) {
 				if (watchField instanceof HTMLInputElement && ["text", "email", "tel", "password"].includes(watchField.type)) {
@@ -33,8 +40,14 @@ export default class FormGroup extends HTMLElement {
 		const showIf = this.getAttribute("showIf") || "";
 		if (value === showIf) {
 			this.setAttribute("show", "");
+			this.fields.forEach((field) => {
+				field.removeAttribute("disabled");
+			});
 		} else {
 			this.removeAttribute("show");
+			this.fields.forEach((field) => {
+				field.setAttribute("disabled", "");
+			});
 		}
 	}
 
