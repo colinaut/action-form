532fc2a6b46e8cb5d08fd4f27bc2847bd179dc7f
Colin Fahrion <colin.fahrion@gmail.com>
2024-03-15 19:30:02 -0700

    feat: group targets name and allows for checkbox groups

--- src/form-group.ts
+++ src/form-group.ts
@@ -1,51 +1,76 @@
+function isChangeField(field: Element): boolean {
+	return (field instanceof HTMLInputElement && ["radio", "checkbox", "range", "color", "file"].includes(field.type)) || field instanceof HTMLSelectElement;
+}
+function isBlurField(field: Element): boolean {
+	return (
+		(field instanceof HTMLInputElement && ["text", "email", "tel", "password", "date", "month", "number", "datetime-local", "time", "url", "week"].includes(field.type)) ||
+		field instanceof HTMLTextAreaElement
+	);
+}
+
 export default class FormGroup extends HTMLElement {
 	constructor() {
 		super();
 	}
 
-	private fields = this.querySelectorAll("input, select, textarea");
+	private groupFields = this.querySelectorAll("input, select, textarea") as NodeListOf<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>;
 
 	public connectedCallback(): void {
 		console.log("connected");
 
 		const form = this.closest("form");
-		const watchId = this.getAttribute("watch");
-		if (form && watchId) {
+		const watch = this.getAttribute("watch");
+		if (form && watch) {
 			// disable all fields in the group
-			this.fields.forEach((field) => {
+			this.groupFields.forEach((field) => {
 				field.setAttribute("disabled", "");
 			});
-			// Set up event listeners
-			const watchField = form.querySelector(`#${watchId}`);
-			if (watchField instanceof HTMLInputElement || watchField instanceof HTMLSelectElement || watchField instanceof HTMLTextAreaElement) {
-				if (watchField instanceof HTMLInputElement && ["text", "email", "tel", "password"].includes(watchField.type)) {
-					watchField.addEventListener("blur", () => {
-						this.show(watchField.value);
+			// Set up event listeners for field matching the name
+			const namedFields = form.querySelectorAll(`:where(input, select, textarea)[name=${watch}]`) as NodeListOf<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>;
+			if (namedFields.length === 0) return;
+
+			namedFields.forEach((field) => {
+				if (isBlurField(field)) {
+					field.addEventListener("blur", () => {
+						this.show(field.value);
 					});
-				} else {
-					watchField.addEventListener("change", () => {
-						if (watchField instanceof HTMLInputElement && watchField.matches("[type=checkbox], [type=radio]")) {
-							const value = watchField.checked ? "checked" : "";
-							this.show(value);
+				}
+				if (isChangeField(field)) {
+					field.addEventListener("change", () => {
+						if (field.matches("[type=checkbox], [type=radio]")) {
+							const values = Array.from(namedFields)
+								.filter((el) => el instanceof HTMLInputElement && el.checked)
+								.map((el) => el.value);
+							this.show(values);
 						} else {
-							this.show(watchField.value);
+							this.show(field.value);
 						}
 					});
 				}
-			}
+			});
 		}
 	}
 
-	private show(value: string): void {
+	private checkMatches(value: string | string[]): boolean {
 		const showIf = this.getAttribute("showIf") || "";
-		if (value === showIf) {
+		if (Array.isArray(value)) {
+			return value.includes(showIf);
+		} else {
+			return showIf === value;
+		}
+	}
+
+	private show(value: string | string[]): void {
+		// TODO: add regex support and or includes support
+		// TODO: change for checked to showIfChecked
+		if (this.checkMatches(value)) {
 			this.setAttribute("show", "");
-			this.fields.forEach((field) => {
+			this.groupFields.forEach((field) => {
 				field.removeAttribute("disabled");
 			});
 		} else {
 			this.removeAttribute("show");
-			this.fields.forEach((field) => {
+			this.groupFields.forEach((field) => {
 				field.setAttribute("disabled", "");
 			});
 		}
